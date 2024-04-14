77d3cf1644cf41dcad5842b381ebbdc6088c3195
PhucLoc <phuclocub@gmail.com>
2024-04-15 00:07:13 +0700

    form up

---
+++ src/component/aboutMe/aboutMe.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function aboutMe() {
+  return (
+    <div className="flex">
+      <div>
+        <img
+          src="https://scontent.fhan5-9.fna.fbcdn.net/v/t39.30808-6/326970206_509218861347055_3330171925877993895_n.jpg?stp=cp6_dst-jpg&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHEE3Fp4coRf8o3FklwURzlk6H_zwVJI7iTof_PBUkjuHEa4CJvvD-w6uEX5tSlpmsFIHibUn1-KNHcLIR3Ow02&_nc_ohc=y15keLZTousAb6jrtC2&_nc_ht=scontent.fhan5-9.fna&oh=00_AfCddramQCbpZXGp8acGfdk0U_K6OhuQunZZj0rlahIuDg&oe=6621D89D"
+          alt=""
+        />
+      </div>
+      <div>
+        I’m looking for a job on IT field. I’m enjoy making anything efficient
+        and simple and simple for development. I also spend time learning new
+        technologies and best practices to become a better engineer. Please take
+        a look my work below to know how can i do. Thank you !
+      </div>
+    </div>
+  );
+}
